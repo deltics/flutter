5f837e51a56d32e02d6095955983da793a233963
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2021-12-30 08:59:43 +1300

    s2.32 - Answer functions

--- s2_flutter_complete_guide/lib/main.dart
+++ s2_flutter_complete_guide/lib/main.dart
@@ -4,13 +4,16 @@ import 'package:flutter/material.dart';
 // Dart entry point
 void main() => runApp(const App());
 
-
 // The App widget
 class App extends StatelessWidget {
-  const App({Key? key }) : super(key: key);
+  const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    void answerOne() {
+      print('Answered \'One\'');
+    }
+
     var questions = [
       'What\'s your favorite color?',
       'What\'s your favorite animal?',
@@ -19,23 +22,21 @@ class App extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Complete Guide'),
+          title: const Text('Complete Guide'),
         ),
         body: Column(
           children: [
-            Text('The question?'),
+            const Text('The question?'),
             ElevatedButton(
-              child: Text('Answer One'),
-              onPressed: null
-            ),
+                child: const Text('Answer One'), onPressed: answerOne),
             ElevatedButton(
-              child: Text('Answer Two'),
-              onPressed: null
-            ),
+                child: const Text('Answer Two'),
+                onPressed: () => print('Answered \'Two\'')),
             ElevatedButton(
-              child: Text('Answer Three'),
-              onPressed: null
-            ),
+                child: const Text('Answer Three'),
+                onPressed: () {
+                  print('Answered \'Three\'');
+                }),
           ],
         ),
       ),
