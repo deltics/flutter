55cb36b70afa3c2db6a4d04a534b6ea7ba55d6ce
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2021-12-29 14:28:07 +1300

    Initial commit

---
+++ s2_flutter_complete_guide/android/app/src/main/kotlin/com/example/s2_flutter_complete_guide/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.s2_flutter_complete_guide
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ s2_flutter_complete_guide/lib/main.dart
@@ -0,0 +1,16 @@
+// Imports
+import 'package:flutter/material.dart';
+
+// Dart entry point
+void main() {
+  runApp(App());
+}
+
+// The App widget
+class App extends StatelessWidget {
+  Widget build(BuildContext ctx) {
+    return MaterialApp(
+      home: Text('Hello!'),
+    );
+  }
+}
