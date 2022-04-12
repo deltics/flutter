7612a8a5a3a3ff24209ee57f906b85ebfabcf966
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2022-04-12 15:43:52 +1200

    s4.111 - Styling of 'Add transaction' button

--- s4_expenses/lib/widgets/transaction_entry.dart
+++ s4_expenses/lib/widgets/transaction_entry.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class TransactionEntry extends StatefulWidget {
   final Function addFunction;
@@ -11,13 +12,13 @@ class TransactionEntry extends StatefulWidget {
 }
 
 class _TransactionEntryState extends State<TransactionEntry> {
-  final titleController = TextEditingController();
-
-  final amountController = TextEditingController();
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime? _transactionDate;
 
   void submit() {
-    final title = titleController.text;
-    final amount = double.tryParse(amountController.text);
+    final title = _titleController.text;
+    final amount = double.tryParse(_amountController.text);
 
     if (title.isEmpty || amount == null || amount <= 0) {
       return;
@@ -41,19 +42,53 @@ class _TransactionEntryState extends State<TransactionEntry> {
           children: <Widget>[
             TextField(
               decoration: const InputDecoration(labelText: 'Title'),
-              controller: titleController,
+              controller: _titleController,
               onSubmitted: (_) => submit(),
             ),
             TextField(
               decoration: const InputDecoration(labelText: 'Amount'),
-              controller: amountController,
+              controller: _amountController,
               keyboardType:
                   const TextInputType.numberWithOptions(decimal: true),
               onSubmitted: (_) => submit(),
             ),
+            Row(children: [
+              Expanded(
+                child: Text(_transactionDate == null
+                    ? 'No date selected'
+                    : DateFormat('dd MMM yyyy @ hh:mm')
+                        .format(_transactionDate!)),
+              ),
+              TextButton(
+                child: Text(_transactionDate == null
+                    ? 'Choose date/time'
+                    : 'Change date/time'),
+                style: TextButton.styleFrom(
+                    primary: Theme.of(context).primaryColor),
+                onPressed: () {
+                  showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(2022),
+                    lastDate: DateTime.now(),
+                  ).then((dt) {
+                    if (dt == null) {
+                      return;
+                    }
+
+                    setState(() {
+                      _transactionDate = dt;
+                    });
+                  });
+                },
+              ),
+            ]),
             TextButton(
               onPressed: () => submit(),
-              style: TextButton.styleFrom(primary: Colors.green),
+              style: TextButton.styleFrom(
+                backgroundColor: Theme.of(context).primaryColor,
+                primary: Colors.white,
+              ),
               child: const Text('Add Transaction'),
             ),
           ],
